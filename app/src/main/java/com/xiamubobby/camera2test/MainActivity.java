afb1449df56a1f8b5c2d202579d4586d5ea6c2d6
xiamubobby <xiamubobby@gmail.com>
2014-11-04 15:20:27 +0800

    nowitbegins

--- app/src/main/java/com/xiamubobby/camera2test/MainActivity.java
+++ app/src/main/java/com/xiamubobby/camera2test/MainActivity.java
@@ -1,150 +1,66 @@
 package com.xiamubobby.camera2test;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.StreamConfigurationMap;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-import android.view.TextureView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 
-import java.util.Arrays;
 
 public class MainActivity extends Activity {
 
-    Size mOutPutSize;
-
-    CameraDevice mCameraDevice;
-    CaptureRequest.Builder mCaptureRequestBuilder;
-    CameraCaptureSession mCameraCaptureSession;
-    TextureView mTextureView;
-    SurfaceTexture mSurfaceTexture;
-    Surface mSurface;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextureView = (TextureView) findViewById(R.id.simp_texture);
-        mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
-            @Override
-            public void onSurfaceTextureAvailable(SurfaceTexture arg0, int arg1, int arg2) {
-                mSurfaceTexture = arg0;
-                mSurface = new Surface(mSurfaceTexture);
-            }
-            @Override
-            public boolean onSurfaceTextureDestroyed(SurfaceTexture arg0) {
-                return false;
-            }
-            @Override
-            public void onSurfaceTextureSizeChanged(SurfaceTexture arg0, int arg1, int arg2) {}
-            @Override
-            public void onSurfaceTextureUpdated(SurfaceTexture arg0) {}
-        });
-        mSurfaceTexture = mTextureView.getSurfaceTexture();
-    }
-    public void openCamera(View view) {
-        openCamera();
-    }
-    public void openCamera() {
-        try {
-            CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-            String cameraId = manager.getCameraIdList()[0];
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
-            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-            mOutPutSize = map.getOutputSizes(SurfaceTexture.class)[0];
-            Size [] wSize = map.getOutputSizes(SurfaceTexture.class);
-            for (int i = 0; i != wSize.length; i++) {
-                Log.v("size at "+i, wSize[i].toString());
-            }
-            manager.openCamera(
-                    cameraId,
-                    new CameraDevice.StateCallback() {
-                        @Override
-                        public void onDisconnected(CameraDevice arg0) {}
-                        @Override
-                        public void onError(CameraDevice arg0, int arg1) {}
-                        @Override
-                        public void onOpened(CameraDevice arg0) {
-                            mCameraDevice = arg0;
-                            /**
-                             * =>createCaptureRequest
-                             */
-                            createCaptureRequest();
-                        }},
-                    null);
-        }
-        catch (CameraAccessException e) {
-            e.printStackTrace();
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
         }
     }
 
-    public void createCaptureRequest() {
-        try {
-            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-        mCaptureRequestBuilder.set(CaptureRequest.CONTROL_SCENE_MODE, CaptureRequest.CONTROL_SCENE_MODE_BARCODE);
-        mCaptureRequestBuilder.addTarget(mSurface);
-        /**
-         * =>createCaptureSession
-         */
-        createCaptureSession();
-    }
 
-    public void createCaptureSession() {
-        mSurfaceTexture.setDefaultBufferSize(mOutPutSize.getWidth(), mOutPutSize.getHeight());
-        try {
-            mCameraDevice.createCaptureSession(
-                    Arrays.asList(mSurface),
-                    new CameraCaptureSession.StateCallback() {
-                        @Override
-                        public void onConfigured(CameraCaptureSession session) {
-                            mCameraCaptureSession = session;
-                            /**
-                             * =>createRepeatRequest
-                             */
-                            createRepeatRequest();
-                        }
-                        @Override
-                        public void onConfigureFailed(CameraCaptureSession session) {
-                            Log.e("cameraConfig", "Failed");
-                        }},
-                    null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createRepeatRequest() {
-        try {
-            mCameraCaptureSession.setRepeatingRequest(mCaptureRequestBuilder.build(), new CameraCaptureSession.CaptureCallback() {
-                @Override
-                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (mCameraDevice == null) {
-            return;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        mCameraDevice.close();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
     }
 }
-
---
+++ app/src/main/java/com/xiamubobby/camera2test/MainActivity2.java
@@ -0,0 +1,150 @@
+package com.xiamubobby.camera2test;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+
+import java.util.Arrays;
+
+public class MainActivity2 extends Activity {
+
+    Size mOutPutSize;
+
+    CameraDevice mCameraDevice;
+    CaptureRequest.Builder mCaptureRequestBuilder;
+    CameraCaptureSession mCameraCaptureSession;
+    TextureView mTextureView;
+    SurfaceTexture mSurfaceTexture;
+    Surface mSurface;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        mTextureView = (TextureView) findViewById(R.id.simp_texture);
+        mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture arg0, int arg1, int arg2) {
+                mSurfaceTexture = arg0;
+                mSurface = new Surface(mSurfaceTexture);
+            }
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture arg0) {
+                return false;
+            }
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture arg0, int arg1, int arg2) {}
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture arg0) {}
+        });
+        mSurfaceTexture = mTextureView.getSurfaceTexture();
+    }
+    public void openCamera(View view) {
+        openCamera();
+    }
+    public void openCamera() {
+        try {
+            CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = manager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            mOutPutSize = map.getOutputSizes(SurfaceTexture.class)[0];
+            Size [] wSize = map.getOutputSizes(SurfaceTexture.class);
+            for (int i = 0; i != wSize.length; i++) {
+                Log.v("size at "+i, wSize[i].toString());
+            }
+            manager.openCamera(
+                    cameraId,
+                    new CameraDevice.StateCallback() {
+                        @Override
+                        public void onDisconnected(CameraDevice arg0) {}
+                        @Override
+                        public void onError(CameraDevice arg0, int arg1) {}
+                        @Override
+                        public void onOpened(CameraDevice arg0) {
+                            mCameraDevice = arg0;
+                            /**
+                             * =>createCaptureRequest
+                             */
+                            createCaptureRequest();
+                        }},
+                    null);
+        }
+        catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createCaptureRequest() {
+        try {
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        mCaptureRequestBuilder.set(CaptureRequest.CONTROL_SCENE_MODE, CaptureRequest.CONTROL_SCENE_MODE_BARCODE);
+        mCaptureRequestBuilder.addTarget(mSurface);
+        /**
+         * =>createCaptureSession
+         */
+        createCaptureSession();
+    }
+
+    public void createCaptureSession() {
+        mSurfaceTexture.setDefaultBufferSize(mOutPutSize.getWidth(), mOutPutSize.getHeight());
+        try {
+            mCameraDevice.createCaptureSession(
+                    Arrays.asList(mSurface),
+                    new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(CameraCaptureSession session) {
+                            mCameraCaptureSession = session;
+                            /**
+                             * =>createRepeatRequest
+                             */
+                            createRepeatRequest();
+                        }
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession session) {
+                            Log.e("cameraConfig", "Failed");
+                        }},
+                    null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createRepeatRequest() {
+        try {
+            mCameraCaptureSession.setRepeatingRequest(mCaptureRequestBuilder.build(), new CameraCaptureSession.CaptureCallback() {
+                @Override
+                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCameraDevice == null) {
+            return;
+        }
+        mCameraDevice.close();
+    }
+}
+
